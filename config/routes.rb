b0ffc4adc7b9b23587e700c1e30bcae77abe2058
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-01-31 11:24:09 +0100

    generate pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
+  root "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
     devise_scope :user do
       get 'users', to: 'devise/sessions#new'
